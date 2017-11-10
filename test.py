7ce143c66bccce98d8bb417b8fe10764b7bc48c3
breimold <reimoldb@gmail.com>
2017-11-10 14:38:15 -0500

    My first Django App, first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+name = 'Sonja'
+if name == "Ola":
+    print("Hey Ola!")
+elif name =="Sonja":
+    print('Hey Sonja!')
+else:
+    print("Hey anonymous!")
+
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+hi("Ola")
